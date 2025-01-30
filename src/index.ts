bf99f3523e3a9a684dfc5a1b628a420d68236f9a
Govard Barkhatov <gbarkhatov@gmail.com>
2025-01-30 10:32:57 +0200

    feat: Support Nested SegWit and Legacy (#56)
    * support nested segwit and legacy
    
    * Rename getInputWitnessUtxo.ts to getInputWitnessUTXO.ts
    
    * tapInternalKey p2tr
    
    * rm getInputWitnessUTXO
    
    * rm unused
    
    * rename to psbtinputfields
    
    * PsbtInputExtended
    
    * test
    
    * revert comment
    
    * bump canary

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-export * from "./utils/btc";
-export * from "./staking/transactions";
-export * from "./types";
-export { ObservableStaking, ObservableStakingScriptData } from "./staking/observable";
 export { Staking, StakingScriptData } from "./staking";
 export type { StakingScripts } from "./staking";
+export {
+  ObservableStaking,
+  ObservableStakingScriptData,
+} from "./staking/observable";
+export * from "./staking/transactions";
+export * from "./types";
+export * from "./utils/btc";
+export * from "./utils/utxo/findInputUTXO";
+export * from "./utils/utxo/getPsbtInputFields";
+export * from "./utils/utxo/getScriptType";
