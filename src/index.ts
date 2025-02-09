3c98f78e8054ea0a749eba81fb7af3af05a7070b
wjrjerome <wjrjerome@babylonchain.io>
2025-02-10 00:00:03 +1100

    fix: remove event emitter replace with signing step

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,8 @@ export * from "./utils/btc";
 export * from "./utils/utxo/findInputUTXO";
 export * from "./utils/utxo/getPsbtInputFields";
 export * from "./utils/utxo/getScriptType";
+export {
+  getBabylonParamByBtcHeight,
+  getBabylonParamByVersion,
+} from "./utils/staking/param";
 export * from "./staking/manager";
